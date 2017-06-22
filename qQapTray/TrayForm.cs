29f91440ddac3ac494300d5be61b7881c97b5a71
Miroslaw Oltuszyk <o2shik@users.noreply.github.com>
2017-06-22 23:32:21 +0100

    Adding Tray project and NLog

---
+++ qQapTray/TrayForm.cs
@@ -0,0 +1,15 @@
+﻿using NLog;
+using System.Windows.Forms;
+
+namespace qQapTray
+{
+    public partial class TrayForm : Form
+    {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+        public TrayForm()
+        {
+            InitializeComponent();
+            _logger.Debug("Tray form initialization");
+        }
+    }
+}
