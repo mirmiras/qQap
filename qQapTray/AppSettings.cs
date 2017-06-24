ad109abab4466e18e428ed61660c2866640c189d
Miroslaw Oltuszyk <o2shik@users.noreply.github.com>
2017-06-24 11:28:36 +0100

    Adding load/save custom configuration

---
+++ qQapTray/AppSettings.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace qQapTray
+{
+    public class AppSettings<T> where T : new()
+    {
+        private const string DefaultFilename = "app.json";
+
+        public void Save(string fileName = DefaultFilename)
+        {
+
+            File.WriteAllText(fileName, JsonConvert.SerializeObject(this, Formatting.Indented));
+        }
+
+        public static void Save(T pSettings, string fileName = DefaultFilename)
+        {
+            File.WriteAllText(fileName, JsonConvert.SerializeObject(pSettings, Formatting.Indented));
+        }
+
+        public static T Load(string fileName = DefaultFilename)
+        {
+            T t = new T();
+            if (File.Exists(fileName))
+            {
+                var readAllText = File.ReadAllText(fileName);
+                t = (T)JsonConvert.DeserializeObject(readAllText, typeof(T));
+            }
+            return t;
+        }
+    }
+}
